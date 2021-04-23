3bcee827b5a9c7276823cc0a4c50b69da74d4817
Swapnil Mitra <swpnlmitra@gmail.com>
2021-04-23 19:13:20 +0530

    new local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -124,3 +124,9 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("No local file. Must be in production")
